78c839cc760697b220c9c8b040622e7470a65db6
HyperPaint <dioman172@ya.ru>
2026-04-03 16:13:28 +1000

    version 26.3.23

--- docker-build.sh
+++ docker-build.sh
@@ -3,6 +3,6 @@
 repository="hyperpaint"
 name="xray-core"
 version="1.0.0"
-xray_core_version="26.2.6"
+xray_core_version="26.3.23"
 
 docker build --build-arg XRAY_CORE_VERSION="v${xray_core_version}" --tag "$repository/$name:$version-$xray_core_version" .
